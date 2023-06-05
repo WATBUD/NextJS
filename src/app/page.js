2b5711dc01a7b321ef7793523d0039275a7fd6c5
水靈 <z23320785@gmail.com>
2023-06-05 13:48:24 +0800

    regular update

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Image from 'next/image'
+//import GoogleLoginButton from '../pages/GoogleLoginButton';
 
-export default function Home() {
+ function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -109,5 +110,17 @@ export default function Home() {
         </a>
       </div>
     </main>
+  )
+}
+
+
+export default function Page() {
+  
+  return (
+  
+  <h1>Hello, Next.js!
+
+  </h1>
+  
   )
 }
