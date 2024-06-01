a7f2d47f9532b6ea6ae6bfddb2bba207624ab818
水靈 <z23320785@gmail.com>
2024-06-01 23:09:55 +0800

    add         removeConsole: true,

--- shadcn_next14/next.config.mjs
+++ shadcn_next14/next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
     //basePath: '/public',
     reactStrictMode:false,//會執行兩次render
     swcMinify:true,
+    compiler: {
+        //removeConsole: process.env.NODE_ENV === "production",
+        removeConsole: true,
+
+    },
     async rewrites() {
         return [
         {
