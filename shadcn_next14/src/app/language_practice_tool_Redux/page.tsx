295325b7b9203ee13cb0778d70700e2c7a002c67
水靈 <z23320785@gmail.com>
2024-10-01 14:10:54 +0800

    Update the file name and the JSON file element.

--- shadcn_next14/src/app/language_practice_tool_Redux/page.tsx
+++ shadcn_next14/src/app/language_practice_tool_Redux/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ClientApp from './ClientApp'; 
+import ClientApp from './client-app'; 
 import { Metadata } from 'next';
 import { CommonMetadata }  from '../common/languageComponent';
 
