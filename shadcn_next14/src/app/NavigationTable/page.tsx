961515a92bf98c4f2d251b426a751a6538fda3e7
水靈 <z23320785@gmail.com>
2024-12-02 16:10:42 +0800

    change homepage

---
+++ shadcn_next14/src/app/NavigationTable/page.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Link from 'next/link';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+const links = [
+  { href: '/Test1', text: 'Test1' },
+  { href: '/stockDashboard', text: 'stockDashboard' },
+  { href: '/payments', text: 'payments' },
+  { href: '/resume/index', text: 'personalResume' },
+  { href: '/language_practice', text: 'language_practice' },
+  { href: '/language_practice_redux', text: 'language_practice_redux' }
+];
+
+export function NavigationTable() {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Name</TableHead>
+          <TableHead>Link</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {links.map((link, index) => (
+          <TableRow key={index}>
+            <TableCell className="font-medium">
+              <Link href={link.href}>{link.text}</Link>
+            </TableCell>
+            <TableCell>
+              <Link href={link.href}>{link.href}</Link>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+} 
--- shadcn_next14/src/app/page.tsx
+++ shadcn_next14/src/app/page.tsx
@@ -1,56 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import Link from 'next/link'
-
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table"
-
-const links = [
-  { href: '/Test1', text: 'Test1' },
-  { href: '/stockDashboard', text: 'stockDashboard' },
-  { href: '/payments', text: 'payments' },
-  { href: '/resume/index', text: 'personalResume' },
-  { href: '/language_practice', text: 'language_practice' },
-  { href: '/language_practice_redux', text: 'language_practice_redux' }
-
-
-
-];
+import LanguagePracticeRedux from '../app/language_practice_redux/page';
 
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Table>
-        {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">Name</TableHead>
-            <TableHead>link</TableHead>
-            {/* <TableHead>link</TableHead> */}
-            {/* <TableHead className="text-right">Amount</TableHead> */}
-          </TableRow>
-        </TableHeader>
-
-        {links.map((link, index) => (
-          <TableBody key={index}>
-            <TableRow>
-              <TableCell className="font-medium">
-                <Link href={link.href}>{link.text}</Link>
-              </TableCell>
-              <TableCell>
-                <Link href={link.href}>{link.text}</Link>
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        ))}
-      </Table>
+      <LanguagePracticeRedux />
     </main>
   );
 }
