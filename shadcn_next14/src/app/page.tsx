c721c8d0b6d05db48e320f71198743937928da8b
水靈 <z23320785@gmail.com>
2024-12-04 11:00:38 +0800

    reomove p 24

--- shadcn_next14/src/app/page.tsx
+++ shadcn_next14/src/app/page.tsx
@@ -3,7 +3,7 @@ import LanguagePracticeRedux from '../app/language_practice_redux/page';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <LanguagePracticeRedux />
     </main>
   );
