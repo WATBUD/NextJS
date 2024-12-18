116655160478150065ce7dfa9e20eacc41201fdd
水靈 <z23320785@gmail.com>
2024-12-18 19:41:31 +0800

    update google-ad-pc.tsx

--- shadcn_next14/src/app/page.tsx
+++ shadcn_next14/src/app/page.tsx
@@ -10,7 +10,8 @@ export default function Home() {
   const isMobile = useIsMobile();
   return (
     <>
-          {!isMobile && (
+        <GoogleAd adClient="ca-pub-5036446798216533" adSlot="4679744551" />
+        {!isMobile && (
         <div
           style={{
             position: "fixed",
@@ -19,17 +20,17 @@ export default function Home() {
             left: "0",
           }}
         >
-          <GoogleAdPC adClient="ca-pub-5036446798216533" adStyle={{
+        <GoogleAdPC adClient="ca-pub-5036446798216533" adStyle={{
             width: '120px',
             height: '720px', 
-            maxHeight: '100%', 
-            }} adSlot="2939969664" />        </div>
+            //maxHeight: '100%', 
+        }} adSlot="2939969664" />        </div>
       )}
       {!isMobile && (
         <div
           style={{
             position: "fixed",
-            backgroundColor: '#000',
+            backgroundColor: '#0000',
             zIndex: 9999,
             right: "0",
           }}
@@ -37,11 +38,10 @@ export default function Home() {
           <GoogleAdPC adClient="ca-pub-5036446798216533" adStyle={{
             width: '120px',
             height: '720px', 
-            maxHeight: '100%', 
+            //maxHeight: '100%', 
             }} adSlot="2939969664" />
         </div>
       )}
-      {/* <GoogleAd adClient="ca-pub-5036446798216533" adSlot="4679744551" /> */}
       <LanguagePracticeRedux />
       {!isMobile && (
         <div
